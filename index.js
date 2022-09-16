0d523929acd4b1833f1c436c9c027ff26eb59092
Soroush Bahrami <sbahrami41@yahoo.com>
2022-09-15 20:00:33 -0400

    Types and Schemas Completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import schema from './schema'
+import resolvers from './resolvers'
 
 const app = express();
 
@@ -8,15 +9,9 @@ app.get('/', (req, res) => {
     res.send('GraphQL')
 });
 
-const root = { product: () => {
-    return{
-        "id": 1,
-        "name": "The Golden Bough",
-        "description": "A Comparative Study of Magic and Religion",
-        "price": 5.00,
-        "soldout": false
-    }
-}}
+
+
+const root = resolvers;
 
 app.use('/graphql' , graphqlHTTP({
     schema: schema,
